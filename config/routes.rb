89cfa27f935588971e28670b12b6a6048187d218
Gothu <mayovwhite@gmail.com>
2020-03-20 01:50:01 +0900

    new-books

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'books#index'
   resources :users, only: [:edit, :update]
-  resources :books, only: [:index, :new, :create]
+  resources :books, only: [:index, :new, :create, :show]
 end

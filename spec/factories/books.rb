da8ddc7412574984d2a49294738fe5b4012eac2e
Gothu <mayovwhite@gmail.com>
2020-04-17 19:22:42 +0900

    test-controller

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -5,9 +5,10 @@ FactoryBot.define do
     comment     {"あああ"}
     image       {File.open("#{Rails.root}/spec/fixtures/test_image.jpg")}
     writer      {"いいい"}
-    price       {"333"}
-    stock       {"222"}
+    price       {333}
+    stock       {222}
     category_id {1}
+    created_at  { Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
     category
     user
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
 
   factory :user do
-    nickname              {"Gothu"}
-    email                 {"test@gmail.com"}
+    nickname              {Faker::Name.name}
     password              {"00000000"}
     password_confirmation {"00000000"}
+    sequence(:email)      {Faker::Internet.email}
   end
 
 end

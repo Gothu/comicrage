a5cf7b3cca5aff7c00bb5589718b5f6e170d830e
Gothu <mayovwhite@gmail.com>
2020-04-20 01:33:04 +0900

    search

--- app/controllers/books/searches_controller.rb
+++ app/controllers/books/searches_controller.rb
@@ -2,9 +2,9 @@ class Books::SearchesController < ApplicationController
 
   def index
     if params[:title_key]
-      @books = current_user.books.where('title LIKE?', "%#{params[:title_key]}%")
+      @books = current_user.books.where('title LIKE?', "%#{params[:title_key]}%").order("created_at DESC").page(params[:page]).per(35)
     else
-      @books = current_user.books.all
+      @books = current_user.books.all.order("created_at DESC").page(params[:page]).per(35)
     end
   end
 

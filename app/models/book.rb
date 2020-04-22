b8ad1743ca9393a199de8a7ac51ffcd8bb2c2085
Gothu <mayovwhite@gmail.com>
2020-04-23 08:40:35 +0900

    remove_image

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,14 @@ class Book < ApplicationRecord
 
   validates :title, :category_id, presence: true
   mount_uploader :image, ImageUploader
+  before_destroy :remove_image
+
+  private
+
+  def remove_image
+    image.remove!
+    image.thumb.remove! # サムネイルを持っていない場合は不要
+  rescue Exception => e
+    logger.error(e.message)
+  end
 end
